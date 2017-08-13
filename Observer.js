e71110b75a5c75e124e44a600c7d0d09b8161be4
Murphy <cx@MurphydeiMac.local>
2017-08-13 22:33:18 +0800

    fixed

---
+++ Observer.js
@@ -0,0 +1,105 @@
+/**
+ * observer
+ */
+
+
+const observer = (data) => {
+	if (!data || typeof data !== 'object') return
+	Object.keys(data).forEach(function(key) {
+		defineReactive(data, key, data[key])
+	})
+}
+
+const defineReactive = (data, key, value) => {
+	const dep = new Dep()
+	observer(value)
+	Object.defineProperty(data, key, {
+		enumerable: true,
+		configurable: false,
+		get: function() {
+			Dep.target && dep.addDep(Dep.target)
+			return value
+		},
+		set: function(newValue) {
+			value = newValue
+			dep.notify()
+			console.log('newValue: ' + value)
+		}
+	})
+}
+/*--------------------- 订阅器 ------------------*/
+function Dep() {
+	this.subs = []
+}
+Dep.prototype = {
+	addSub: function(sub) {
+		this.subs.push(sub)
+	},
+	notify: function() {
+		this.subs.forEach(function(sub) {
+			sub.update()
+		})
+	}
+};
+
+function Watcher(vm, key, cb) {
+	this.cb    = cb
+	this.vm    = vm
+	this.key   = key
+	this.value = this.get()
+}
+
+Watcher.prototype = {
+	get() {
+		Dep.target  = this
+		const value = data[key]
+		Dep.target  = null
+		return value
+	},
+	update() {
+		this.run()
+	},
+	run() {
+		const value = this.get()
+		const oldValue =  this.value
+		if (value !== oldValue) {
+			this.value   = oldValue
+			this.cb.call(this.vm, value, oldValue)
+		}
+	}
+}
+
+function Compile(el) {
+	this.$el = el
+	if (this.$el) {
+		this.$fragment = this.nodeToFragment(this.$el)
+		this.init()
+		this.$el.appendChild(this.$fragment)
+	}
+}
+
+Compile.prototype = {
+	init() {
+		this.compileElement(this.$fragment)
+	},
+	nodeToFragment(el) {
+		const fragment = document.createDocumentFragment()
+		let child = null
+		while (child = el.firstChild) {
+			fragment.appendChild(child)
+		}
+		return fragment
+	},
+}
+
+
+
+
+// const data = {
+// 	value: 'hello'
+// }
+// observer(data)
+//
+// setTimeout(() => {
+// 	data.value = 'world' // output: value: hello    newValue: world
+// }, 2000)
